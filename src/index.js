fde70e5d352459c0d9cb5ce65e8a256ef067af85
ilaksono <ian.laksono@gmail.com>
2020-12-27 23:23:45 -0500

    add axios url

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import 'styles/Animations.scss';
 // core components
 import Admin from "layouts/Admin.js";
-
+import axios from 'axios';
 import "styles/css/material-dashboard-react.css?v=1.9.0";
 import { AppProvider } from 'AppContext';
 import { CookiesProvider } from 'react-cookie';
 
-
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 const hist = createBrowserHistory();
 ReactDOM.render(
   <React.StrictMode>
